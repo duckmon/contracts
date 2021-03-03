6d0199f7d8a91e567a6fb4e1486569e0052cd3b1
Code <code@localhost>
2021-03-03 23:32:17 +0100

    Duck contracts

--- contracts/DuckToken.sol
+++ contracts/DuckToken.sol
@@ -1,9 +1,65 @@
+//                                                                                        .                
+//                                         #@#########@@@@                                                 
+//                                      @@@&#########@@@@@@@@#                                             
+//                                    @@@@@@@#######@@@@@@@@@@@,                                           
+//                                  /@@@@@@@@@#####%@@@@@@@@@@@                                            
+//                                  @@@@@@@@@%######@@@@@%#@@@                                             
+//                                   @@@@@#@@@@#####@@@@@@@@&                                              
+//                                    %@%@@@@@@#####@@@@@@@@@                                              
+//                                     #@@@@@@@###%#%@@@@@@@@@@@@@%(                                       
+//                                      @@@@@#%@@ .&@@@@@@@@@@@@@@@@@&                                     
+//                                   *#%#&@@@@@.      @@@@@@@@@@@@@@@@&                                    
+//                            @@@@@@@@   &@@@%(   &@   @ &@@@@@@@@@@@@@                                    
+//                            @@@@@@@@ *  &   %  .   &  %    @@@@@@@@@(                                    
+//                             &@@@@@, ,& .   . *     &        @@@@@@/                                     
+//                               (@@& @  % ,   %      /         %@%                                        
+//                          # /@# & .%%  /  /  ( %@@                                                       
+//                              @,    %*(@  &    #@@.  .       .,  ,(                                      
+//                         %    %%  * **,@/&  ,  &@&  ,        #%%   ,%%                                   
+//                         #,,,,,,,,*(,  .(,,,,#      &       /,*,      ,                                  
+//                         &,,%&/(%*,,,,,,,,,,,,,,/, .   .###. # *     .&                                  
+//                            ,*,,,,,,,(,,,,,,,,,,,,,,,,,,*(/,*,,/    %/#                                  
+//  .                             #,,,,,,,,(/,,,,,%&*,,,,,,*%,,,,,%   #*                                   
+//                               #(.   ./##*,,,,,/%@@@.       &  . *#/                                     
+//                                , & /.#%,%(#.(((  .(((&                                                  
+//                                      %(((%      &(((((%                                                 
+//                                     ((((((##&(((((((((&(((%(                                            
+//                                   /#((((((%@((((((((((@((&((%#*                                         
+//              &@@%%%%%&@@@       &((%((((@  &%((((((((#(#((#%&((((@                                      
+//            /&&&@@%%%%%%&       %@((((#((*   @@%((((%##(#((((((((((%((((((%#                             
+//            &%%%%@%%%%%@       %%((((((((((%((((((((((((((((((((#(((((((((((((%                          
+//            (     .  .@*  @   &(#(((((((((((((((((((#@@ .*%((((%(((((((#       .@*                       
+//           @  ,@     /   #&((#(&(((((((((((((((((((((((#@     #%(((((&@,       ,  ,  &                   
+//           *,     / *%&@@ %((%(&((((((((((((((((((((((((((@    (#((((@(@   @&%@   .  (      &            
+//           #      /  .@%&%@#(((#(((((((((((((((((((((((((@%(%*      .  /  @& /   #  #    .               
+//           &%@@@&#&@&&%&%%%%%@@&(((((((((((((((((((((((@%#(((((#      %@@&%   ,&%@&                      
+//         *%%%%&%%%%%%%%%&%%%%%%@@((((((((((((((((((#&%@(((((((((%       %%/   @%%@                       
+//        &%%%%%%%&%%%%%%%%%%%%%&#(((%%%%%%#%%&&&%%&%((((((((((((((%     .%%                               
+//       @%%%%%%%%&@%%%%%%%%%%%%@((((((((((@((((((((((((((((((((((((     @%&                               
+//      &%&%%%%&%%%%%%%%%%%%%%%@@((((((((((@ %((((((((((((((((((((((,    &%.                               
+//     .%%%%%%%%&&,.&@%%%%%%%%%@@((((((((((#  %(((((((((((((((((((((     %%                                
+//     (%%%%%%%@%%%%%%&%%%%%%%%%@((((((((((@   ,(((((((((((((((((((@    ,%&                                
+//     (%%%%%%%#%%@%%%%%%%%%%%%%@@(((((((((@    .(((((((((((((((((@     %%*                                
+//      %%@%%%%%%%%%%%%%%%%%%%%%%@@(((((((((@    ,(((((((((((((((&      %%                                 
+//       &%%&&%%%%%%%%%%%%%%%%%%%&%@%(((((((@@%&@@,((((((((((((@        %&                                 
+//        @%%@@&&&%%%%%%%%%%%@%%%%   /&@&%#&,%((*   &(((((((%*,,%%     ,%(                                 
+//          (@%%%&&&@@%%&&@@%%%%    #(((##(((((#      *@%.(&*,*(@(((   @%                                  
+//                *#(&#&#(           .@%#(#%&@@%         %((((((((@    %&                                  
+//                                    @((((((((((((#/      .@@@@@((#@ .%/                                  
+//                          &(((#%(((((((((((((((((((#    %(((((((((((&%(%&%/                              
+//                       %,,,,,@((((#(((((((((((((((,%   &@(((((((((((%&((((((((((&                        
+//                      &,,,,,,,,#((((((((((((((((#%      @(((((((((((%&((((#*,,,,/&                       
+//                      .,,,,,,,,,,,%((((((((%(((&         ##(@((((((%%((#,,,,,,,,,,/                      
+//                        ,%%(#,,,,,,,,,,,,/*                 @,,%&%%%&,,,,,,,,,,,@,                       
+//                                                               &#,,%(,,,,,,,,,,(                         
+//                                                                  #%.                                    
+//                                                                  #&                                     
+
 pragma solidity 0.6.12;
 
 import "./libs/BEP20.sol";
 
-// EggToken with Governance.
-contract EggToken is BEP20('Goose Golden Egg', 'EGG') {
+contract DuckToken is BEP20('Duck.Money Token', 'QUACK') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
@@ -112,9 +168,9 @@ contract EggToken is BEP20('Goose Golden Egg', 'EGG') {
         );
 
         address signatory = ecrecover(digest, v, r, s);
-        require(signatory != address(0), "EGG::delegateBySig: invalid signature");
-        require(nonce == nonces[signatory]++, "EGG::delegateBySig: invalid nonce");
-        require(now <= expiry, "EGG::delegateBySig: signature expired");
+        require(signatory != address(0), "DUCK::delegateBySig: invalid signature");
+        require(nonce == nonces[signatory]++, "DUCK::delegateBySig: invalid nonce");
+        require(now <= expiry, "DUCK::delegateBySig: signature expired");
         return _delegate(signatory, delegatee);
     }
 
@@ -144,7 +200,7 @@ contract EggToken is BEP20('Goose Golden Egg', 'EGG') {
         view
         returns (uint256)
     {
-        require(blockNumber < block.number, "EGG::getPriorVotes: not yet determined");
+        require(blockNumber < block.number, "DUCK::getPriorVotes: not yet determined");
 
         uint32 nCheckpoints = numCheckpoints[account];
         if (nCheckpoints == 0) {
@@ -181,7 +237,7 @@ contract EggToken is BEP20('Goose Golden Egg', 'EGG') {
         internal
     {
         address currentDelegate = _delegates[delegator];
-        uint256 delegatorBalance = balanceOf(delegator); // balance of underlying EGGs (not scaled);
+        uint256 delegatorBalance = balanceOf(delegator); // balance of underlying DUCKs (not scaled);
         _delegates[delegator] = delegatee;
 
         emit DelegateChanged(delegator, currentDelegate, delegatee);
@@ -217,7 +273,7 @@ contract EggToken is BEP20('Goose Golden Egg', 'EGG') {
     )
         internal
     {
-        uint32 blockNumber = safe32(block.number, "EGG::_writeCheckpoint: block number exceeds 32 bits");
+        uint32 blockNumber = safe32(block.number, "DUCK::_writeCheckpoint: block number exceeds 32 bits");
 
         if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
             checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
